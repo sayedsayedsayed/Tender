b89b781fd6701b1d7048a2c4ad2bb51647052269
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-05-30 15:38:43 +0700

    Initial Commit

---
+++ Tender/TenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenderApp.swift
+//  Tender
+//
+//  Created by Sayed Zulfikar on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
