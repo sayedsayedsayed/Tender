48961f0d2333dfe64d067eb0708cebb8846948af
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-05-31 08:17:44 +0700

    DB + controller and referencehelper framework

---
+++ Tender/Helpers/ReferenceHelper.swift
@@ -0,0 +1,29 @@
+//
+//  ReferenceHelper.swift
+//  Tender
+//
+//  Created by Sayed Zulfikar on 31/05/23.
+//
+
+import Foundation
+
+func generateReferenceCode(id: Int, counter: Int) -> String{
+    //TODO: create reference code generator based on combination of userid and reference counter
+    var result = ""
+    return result
+}
+
+func decryptReferenceCode(referenceCode: String) -> (Int, Int){
+    //TODO: dechipher the reference code
+    var id = 0
+    var counter = 0
+    return (id, counter)
+}
+
+func checkReferenceCode() -> Bool {
+    //TODO: check if the reference code is valid or not
+    var result = true
+    return result
+}
+
+
