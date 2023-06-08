3991d58bba2aa619ce5e885a7ad65bed6c543f96
agfidprasetyo <agfid11@gmail.com>
2023-06-08 15:31:45 +0700

    remove padding at scroll view List Connected

--- Tender/Views/ConnectedView.swift
+++ Tender/Views/ConnectedView.swift
@@ -19,87 +19,79 @@ struct ConnectedView: View {
     @State private var search: String = ""
     @State private var isPresented: Bool = false
     @Binding var activeScreen: Show
-
+    
     var body: some View {
         VStack {
             ZStack {
                 VStack(spacing: 0) {
-                    ZStack {
-                        VStack {
-                            MenuItem(namespace: namespace, title: "CONNECTED", isHeader: activeScreen == .connected ? true : false, activeScreen: $activeScreen)
-                                .highPriorityGesture(DragGesture(minimumDistance: 30, coordinateSpace: .local)
-                                    .onEnded { value in
-                                        if abs(value.translation.height) > abs(value.translation.width) {
-                                            if value.translation.height > 0 {
-                                                withAnimation() {
-                                                    activeScreen = .menu
-                                                }
-                                            }
+                    MenuItem(namespace: namespace, title: "CONNECTED", isHeader: activeScreen == .connected ? true : false, activeScreen: $activeScreen)
+                        .highPriorityGesture(DragGesture(minimumDistance: 30, coordinateSpace: .local)
+                            .onEnded { value in
+                                if abs(value.translation.height) > abs(value.translation.width) {
+                                    if value.translation.height > 0 {
+                                        withAnimation() {
+                                            activeScreen = .menu
                                         }
                                     }
-                                )
-//                            Spacer()
-                        }
+                                }
+                            }
+                        )
+                    
+                    ScrollView {
+                        ZStack {
+                            if isSearch {
+                                HStack(spacing: 0) {
+                                    SearchBar(search: $search, isSearch: $isSearch)
+                                }.ignoresSafeArea()
+                            } else {
+                                HStack(spacing: 10) {
+                                    Spacer()
+                                    Image(systemName: "magnifyingglass")
+                                        .font(Font.system(.title2))
+                                        .padding(.top, 5)
+                                        .onTapGesture {
+                                            isSearch.toggle()
+                                        }
+                                    Image(systemName: "line.3.horizontal.decrease")
+                                        .font(Font.system(.title2))
+                                        .padding(.top, 5)
+                                        .onTapGesture {
+                                            isPresented.toggle()
+                                        }
+                                }.padding(.horizontal, 30)
+                                    .foregroundColor(Color.black)
+                            }
+                        }.ignoresSafeArea()
+                        ListFreelancer(freelancers: freelancers)
+                        
                     }
-                    ZStack {
-                        ScrollView {
-                            ZStack {
-                                if isSearch {
-                                    HStack(spacing: 0) {
-                                        SearchBar(search: $search, isSearch: $isSearch)
-                                    }.ignoresSafeArea()
-                                } else {
-                                    HStack(spacing: 10) {
+                    .scrollIndicators(.hidden)
+                    .sheet(isPresented: $isPresented) {
+                        VStack {
+                            Text("Filter").font(.headline).fontWeight(.bold).padding(.vertical, 20)
+                            Spacer()
+                            ScrollView {
+                                Accordion(title: "Skills", content: AnyView(ForEach(SkillsViewModel().skills) { skill in
+                                    Checkbox(label: skill.name)
+                                }))
+                                Accordion(title: "Roles", content: AnyView(ForEach(roles) {role in
+                                    Checkbox(label: role.name)
+                                }))
+                                Accordion(title: "Availability", content: AnyView(ForEach(availability.allCases, id: \.rawValue) {role in
+                                    Checkbox(label: role.rawValue)
+                                }))
+                                ZStack {
+                                    VStack {
                                         Spacer()
-                                        Image(systemName: "magnifyingglass")
-                                            .font(Font.system(.title2))
-                                            .padding(.top, 5)
-                                            .onTapGesture {
-                                                isSearch.toggle()
-                                            }
-                                        Image(systemName: "line.3.horizontal.decrease")
-                                            .font(Font.system(.title2))
-                                            .padding(.top, 5)
-                                            .onTapGesture {
-                                                isPresented.toggle()
-                                            }
-                                    }.padding(.horizontal, 30)
-                                        .foregroundColor(Color.black)
-                                }
-                            }.ignoresSafeArea()
-                            ListFreelancer(freelancers: freelancers)
-                            
-                        }
-                        .padding(.bottom, 70)
-                        .scrollIndicators(.hidden)
-                        .sheet(isPresented: $isPresented) {
-                            VStack {
-                                Text("Filter").font(.headline).fontWeight(.bold).padding(.vertical, 20)
-                                Spacer()
-                                ScrollView {
-                                    Accordion(title: "Skills", content: AnyView(ForEach(SkillsViewModel().skills) { skill in
-                                        Checkbox(label: skill.name)
-                                    }))
-                                    Accordion(title: "Roles", content: AnyView(ForEach(roles) {role in
-                                        Checkbox(label: role.name)
-                                    }))
-                                    Accordion(title: "Availability", content: AnyView(ForEach(availability.allCases, id: \.rawValue) {role in
-                                        Checkbox(label: role.rawValue)
-                                    }))
-                                    ZStack {
-                                        VStack {
-                                            Spacer()
-                                            Button {
-                                                print("test")
-                                            } label: {
-                                                Text("Apply")
-                                            }
+                                        Button {
+                                            print("test")
+                                        } label: {
+                                            Text("Apply")
                                         }
                                     }
                                 }
-                            }.presentationDetents([.fraction(0.8)])
-                        }
-
+                            }
+                        }.presentationDetents([.fraction(0.8)])
                     }
                 }
                 
