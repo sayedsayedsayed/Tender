1068e0419e00c9aad199823efeac9d6a33bc6f61
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-06-09 22:03:27 +0700

    add FormTitleWithIcon component

---
+++ Tender/Components/CapsuleSkillView.swift
@@ -0,0 +1,20 @@
+//
+//  CapsuleSkillView.swift
+//  Tender
+//
+//  Created by Ahmad Fadly Iksan on 09/06/23.
+//
+
+import SwiftUI
+
+struct CapsuleSkillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CapsuleSkillView_Previews: PreviewProvider {
+    static var previews: some View {
+        CapsuleSkillView()
+    }
+}
---
+++ Tender/Views/CreateProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateProfileView.swift
+//  Tender
+//
+//  Created by Ahmad Fadly Iksan on 09/06/23.
+//
+
+import SwiftUI
+
+struct CreateProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateProfileView()
+    }
+}
